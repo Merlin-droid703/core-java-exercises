ce8a7f57489e1220b9c5d1478563e8f4ab33c774
Merlin-droid703 <melisa_aladag@hotmail.com>
2025-06-01 14:11:11 +0300

    For döngüsü ile 1'den 10'a kadar sayıları yazdırma eklendi

---
+++ src/BirdenOnaKadar.java
@@ -0,0 +1,13 @@
+public class BirdenOnaKadar public class BirdenOnaKadar {
+
+    public static void main(String[] args) {
+        // for döngüsü:
+        // 1. Başlangıç: int i = 1 (i adında bir sayaç başlatılır ve değeri 1 olur)
+        // 2. Koşul: i <= 10 (döngü, i 10'dan küçük veya eşit olduğu sürece devam eder)
+        // 3. Artış: i++ (her döngü adımından sonra i'nin değeri 1 artırılır)
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
